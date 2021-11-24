499dcf5509771e487fa0dfd877913eb247a0d0bf
sceka <scekicm02@gmail.com>
2021-11-24 10:58:14 +0100

    Add validation chain function to blockchain

--- main.js
+++ main.js
@@ -41,10 +41,33 @@ class BlockChain {
 		newBlock.hash = newBlock.calculateHash();
 		this.chain.push(newBlock);
 	}
+
+	isChainValid() {
+		for (let i = 1; i < this.chain.length; i++) {
+			const currentBlock = this.chain[i];
+			const previousBlock = this.chain[i - 1];
+
+			if (
+				currentBlock.hash !==
+				currentBlock.calculateHash()
+			) {
+				return false;
+			}
+			if (
+				currentBlock.previousHash !==
+				previousBlock.hash
+			) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
 
 let ethCoin = new BlockChain();
 ethCoin.addBlock(new Block(1, '25/11/2021', { amount: 2 }));
 ethCoin.addBlock(new Block(2, '26/11/2021', { amount: 5 }));
 
-console.log(JSON.stringify(ethCoin, null, 4));
+// console.log(JSON.stringify(ethCoin, null, 4));
+
+console.log('Is chain valid? ' + ethCoin.isChainValid());
