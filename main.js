44a5adca5ffea2260aa7889d08111186e0d6aa19
sceka <scekicm02@gmail.com>
2021-11-24 10:13:37 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,8 @@
+class Block {
+	constructor(index, timestamp, data, previousHash) {
+		this.index = index;
+		this.timestamp = timestamp;
+		this.data = data;
+		this.previousHash = previousHash;
+	}
+}
